f6e317d1a24d73e8a83445c3e08b0991884c916b
Diego R. M. Souza <54595784+diegormsouza@users.noreply.github.com>
2022-04-29 14:23:34 -0300

    Add files via upload

--- utilities_ocean.py
+++ utilities_ocean.py
@@ -120,8 +120,6 @@ def download_OCEAN(product, date, path_dest):
 
   #-----------------------------------------------------------------------------------------------------------
 
-  print(product)
-  
   # Access the FTP folder, based on the desired product
   if (product == 'SST'):
       # FTP Path
@@ -271,7 +269,6 @@ def download_OCEAN(product, date, path_dest):
       file_name = naming_convention + '_' + year + extension
 
   elif (product == 'DHW'):
-      print("ESTOU AQUI NO DHW")
       # FTP Path
       path = ('pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/daily/dhw/' + year + '/')
       naming_convention = 'ct5km_dhw_v3.1'
@@ -381,7 +378,6 @@ def download_OCEAN(product, date, path_dest):
 
 #-----------------------------------------------------------------------------------------------------------
   
-  print(path)
   # Enter the FTP Path
   ftp.cwd(path)
 
