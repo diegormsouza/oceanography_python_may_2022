5710b5d4c0766f3d6901343b91ea6f61bbe9f414
Diego R. M. Souza <54595784+diegormsouza@users.noreply.github.com>
2022-04-29 12:49:22 -0300

    Add files via upload

--- utilities_ocean.py
+++ utilities_ocean.py
@@ -40,6 +40,11 @@ def download_OCEAN(product, date, path_dest):
   # BHS Monthly - max (Global - 5 km): ftp.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/monthly
   # BHS Annual - max (Global - 5 km) : ftp.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/annual
 
+  # CORAL BLEACHING HEAT STRESS DEGREE HEATING WEEK:
+  # DHW (Global - 5 km)              : ftp.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/daily/dhw
+  # DHW Monthly - max (Global - 5 km): ftp.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/monthly
+  # DHW Annual - max (Global - 5 km) : ftp.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/annual
+
   # OCEAN COLOR:
   # OC [filled] (Global - 9 km): ftp.star.nesdis.noaa.gov/pub/socd1/mecb/coastwatch/viirs/nrt/L3/global/chlora/dineof
   # OC Granules (S-NPP)        : ftpcoastwatch.noaa.gov/pub/socd1/mecb/coastwatch/viirs/nrt/L2/
@@ -72,7 +77,16 @@ def download_OCEAN(product, date, path_dest):
   dir = path_dest; os.makedirs(dir, exist_ok=True)
 
   # Product selection
-  product = product # options: 'SST', 'SST-A' (Anomaly), 'SST-T' (Trend), 'CLO' (Ocean Color), 'SLA' (Sea Level Anomaly), 'ASW' (ASCAT Winds), 'JAS' (JASON-3)
+  product = product 
+  # Options: 
+  # 'SST', 'SST-A' (Anomaly), 'SST-T' (Trend), 'BAA' (Bleaching Alert), 'BHS' (Bleaching Hotspot), 'DHW' (Degree Heating Week), 'CLO' (Ocean Color), 'SLA' (Sea Level Anomaly), 'ASC-A-a', ASC-A-d, ASC-B-a, ASC-A-d (ASCAT Winds), 'JAS' (JASON-3), 
+  # 'SST-Monthly-Min', 'SST-Monthly-Mean', 'SST-Monthly-Max', 
+  # 'SST-Annual-Min', 'SST-Annual-Mean', 'SST-Annual-Max'
+  # 'SST-A-Monthly-Min', 'SST-A-Monthly-Mean', 'SST-A-Monthly-Max'
+  # 'SST-A-Annual-Min', 'SST-A-Annual-Mean', 'SST-A-Annual-Max'
+  # 'BAA-Monthly-Max', 'BAA-Annual-Max'
+  # 'BHS-Monthly-Max', 'BHS-Annual-Max'
+  # 'DHW-Monthly-Max', 'DHW-Annual-Max'
 
   # Desired year (four digit)
   year = date[0:4]
@@ -90,12 +104,13 @@ def download_OCEAN(product, date, path_dest):
 
   # FTP Address
   if (product == 'SST' or product == 'SST-A' or product == 'SST-T' or product == 'BAA' or 
-      product == 'BHS' or product == 'CLO' or product == 'SST-Monthly-Min' or product == 'SST-Monthly-Mean' or 
+      product == 'BHS' or product == 'DHW' or product == 'CLO' or product == 'SST-Monthly-Min' or product == 'SST-Monthly-Mean' or 
       product == 'SST-Monthly-Max' or product == 'SST-Annual-Min' or product == 'SST-Annual-Mean' or 
       product == 'SST-Annual-Max' or product == 'SST-A-Monthly-Min' or product == 'SST-A-Monthly-Mean' or 
       product == 'SST-A-Monthly-Max' or product == 'SST-A-Annual-Min' or product == 'SST-A-Annual-Mean' or 
       product == 'SST-A-Annual-Max' or product == 'BAA-Monthly-Max' or product == 'BHA-Annual-Max' or
-      product == 'BHS-Monthly-Max' or product == 'BHS-Annual-Max'):
+      product == 'BHS-Monthly-Max' or product == 'BHS-Annual-Max' or 
+      product == 'DHW-Monthly-Max' or product == 'DHW-Annual-Max'):
     ftp = FTP('ftp.star.nesdis.noaa.gov') 
   elif (product == 'ASC-A-a' or 'ASC-A-d' or 'ASC-B-a' or 'ASC-B-d' or product == 'SLA' or product == 'JAS'):
     ftp = FTP('ftpcoastwatch.noaa.gov') 
@@ -253,6 +268,27 @@ def download_OCEAN(product, date, path_dest):
       extension = '.nc'
       file_name = naming_convention + '_' + year + extension
 
+  elif (product == 'DHW'):
+      # FTP Path
+      path = ('pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/daily/dhw/' + year + '/')
+      naming_convention = 'ct5km_dhw_v3.1'
+      extension = '.nc'
+      file_name = naming_convention + '_' + year + month + day + extension
+
+  elif (product == 'DHW-Monthly-Max'):
+      # FTP Path
+      path = ('pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/monthly/' + year + '/')
+      naming_convention = 'ct5km_dhw-max_v3.1'
+      extension = '.nc'
+      file_name = naming_convention + '_' + year + month + extension
+
+  elif (product == 'DHW-Annual-Max'):
+      # FTP Path
+      path = ('pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/annual/')
+      naming_convention = 'ct5km_dwh-max_v3.1'
+      extension = '.nc'
+      file_name = naming_convention + '_' + year + extension
+    
   elif (product == 'CLO'):
       # Converting date to julian day
       import datetime
