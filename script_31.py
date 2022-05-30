81ebd460792feb0373c379859ce0cc126d2ecd8e
Diego R. M. Souza <54595784+diegormsouza@users.noreply.github.com>
2022-05-30 10:12:15 -0300

    Add files via upload

--- script_31.py
+++ script_31.py
@@ -1,146 +1,143 @@
 #---------------------------------------------------------------------------------------------------------------------------
-# INPE / CGCT / DISSM - Training: Oceanography Products - Script 31: Comparing SST (Night and Day)
+# INPE / CGCT / DISSM - Training: Oceanography Products - Script 31.1: Comparing LEO and GEO products
 # Author: Diego Souza (INPE / CGCT / DISSM)
 #---------------------------------------------------------------------------------------------------------------------------
 # Required modules
-from netCDF4 import Dataset                     # Read / Write NetCDF4 files
-import matplotlib.pyplot as plt                 # Plotting library
-from datetime import datetime                   # Basic Dates and time types
-import cartopy, cartopy.crs as ccrs             # Plot maps
-import os                                       # Miscellaneous operating system interfaces
-from osgeo import gdal                          # Python bindings for GDAL
-import numpy as np                              # Scientific computing with Python
-from utilities_goes import download_PROD        # Our function for download
-from utilities_goes import reproject            # Our function for reproject
-gdal.PushErrorHandler('CPLQuietErrorHandler')   # Ignore GDAL warnings
-#-----------------------------------------------------------------------------------------------------------
+from netCDF4 import Dataset                   # Read / Write NetCDF4 files
+import matplotlib.pyplot as plt               # Plotting library
+from datetime import datetime, timedelta      # Basic date and time types
+import cartopy, cartopy.crs as ccrs           # Plot maps
+import cartopy.feature as cfeature            # Common drawing and filtering operations
+import cartopy.io.shapereader as shpreader    # Import shapefiles
+import numpy as np                            # Import the Numpy package
+import matplotlib.colors                      # Matplotlib colors  
+from datetime import datetime, timedelta      # Basic Dates and time types
+import os                                     # Miscellaneous operating system interfaces
+import time as t                              # Time access and conversion                                          
+from ftplib import FTP                        # FTP protocol client
+from utilities_ocean import download_OCEAN    # Our function for download
+from utilities_goes import download_PROD      # Our function for download
+from utilities_goes import reproject          # Our function for reproject
+from osgeo import gdal                        # Python bindings for GDAL
+gdal.PushErrorHandler('CPLQuietErrorHandler') # Ignore GDAL warnings
+#---------------------------------------------------------------------------------------------------------------------------
 # Input and output directories
 input = "Samples"; os.makedirs(input, exist_ok=True)
 output = "Output"; os.makedirs(output, exist_ok=True)
 
+# Time / Date for download
+date = '20220529' # YYYYMMDD
+
+# Download the file (product, date, directory)
+file_leo = download_OCEAN('SST-LEO', date, input)
+#---------------------------------------------------------------------------------------------------------------------------
+# Open the file using the NetCDF4 library
+file_leo = Dataset(f'{input}/{file_leo}')
+#---------------------------------------------------------------------------------------------------------------------------
+# Select the extent [min. lon, min. lat, max. lon, max. lat]
+extent = [-93.0, -60.00, -25.00, 18.00] # South America
+       
+# Reading lats and lons 
+lats = file_leo.variables['lat'][:]
+lons = file_leo.variables['lon'][:]
+
+# Latitude lower and upper index
+latli = np.argmin( np.abs( lats - extent[1] ) )
+latui = np.argmin( np.abs( lats - extent[3] ) )
+ 
+# Longitude lower and upper index
+lonli = np.argmin( np.abs( lons - extent[0] ) )
+lonui = np.argmin( np.abs( lons - extent[2] ) )
+ 
+# Extract the Sea Surface Temperature
+data_leo = file_leo.variables['sea_surface_temperature'][ 0 , latui:latli , lonli:lonui ] - 273.15
+#---------------------------------------------------------------------------------------------------------------------------
 # Parameters to process
+yyyymmdd = date
 product_name = 'ABI-L2-SSTF'
 
-# Desired extent
-#extent = [-140.0, -60.00, -25.00, 18.00] # Min lon, Max lon, Min lat, Max lat
-extent = [-140.0, -60.00, -10.00, 50.00]
+# Select the extent [min. lon, min. lat, max. lon, max. lat]
+extent = [-93.0, -60.00, -25.00, 18.00] # South America
 
-# Date structure
-yyyymmddhhmn = '202201010400'
+########################################################################
+# Sea Surface Temperature - "X" Hours
+########################################################################
 
-# Download the file
-file_name = download_PROD(yyyymmddhhmn, product_name, input)
+sum_ds = np.zeros((5424,5424))
+count_ds = np.zeros((5424,5424))
 #-----------------------------------------------------------------------------------------------------------
-# Variable
-var = 'SST'
+for hour in np.arange(14,20+1,1):
 
-# Open the GOES-R image
-file = Dataset(f'{input}/{file_name}.nc')        
+    # Date structure
+    yyyymmddhhmn = f'{yyyymmdd}{hour:02.0f}00'
 
-# Open the file
-img = gdal.Open(f'NETCDF:{input}/{file_name}.nc:' + var)
-
-# Data Quality Flag (DQF)
-dqf = gdal.Open(f'NETCDF:{input}/{file_name}.nc:DQF')
-
-# Read the header metadata
-metadata = img.GetMetadata()
-scale = float(metadata.get(var + '#scale_factor'))
-offset = float(metadata.get(var + '#add_offset'))
-undef = float(metadata.get(var + '#_FillValue'))
-dtime = metadata.get('NC_GLOBAL#time_coverage_start')
-
-# Load the data
-ds = img.ReadAsArray(0, 0, img.RasterXSize, img.RasterYSize).astype(float)
-ds_dqf = dqf.ReadAsArray(0, 0, dqf.RasterXSize, dqf.RasterYSize).astype(float)
-
-# Apply the scale, offset and convert to celsius
-ds = (ds * scale + offset) - 273.15
-
-# Apply NaN's where the quality flag is greater than 1
-ds[ds_dqf > 1] = np.nan
-#-----------------------------------------------------------------------------------------------------------
+    # Download the file
+    file_name = download_PROD(yyyymmddhhmn, product_name, input)
+    #-----------------------------------------------------------------------------------------------------------
+    # Variable
+    var = 'SST'
+    # Open the GOES-R image
+    file = Dataset(f'{input}/{file_name}.nc')        
+    # Open the file
+    img = gdal.Open(f'NETCDF:{input}/{file_name}.nc:' + var)
+    # Data Quality Flag (DQF)
+    dqf = gdal.Open(f'NETCDF:{input}/{file_name}.nc:DQF')
+    # Read the header metadata
+    metadata = img.GetMetadata()
+    scale = float(metadata.get(var + '#scale_factor'))
+    offset = float(metadata.get(var + '#add_offset'))
+    undef = float(metadata.get(var + '#_FillValue'))
+    dtime = metadata.get('NC_GLOBAL#time_coverage_start')
+    # Load the data
+    ds = img.ReadAsArray(0, 0, img.RasterXSize, img.RasterYSize).astype(float)
+    ds_dqf = dqf.ReadAsArray(0, 0, dqf.RasterXSize, dqf.RasterYSize).astype(float)
+    # Apply the scale, offset and convert to celsius
+    ds = (ds * scale + offset) - 273.15
+    # Apply NaN's where the quality flag is greater than 1
+    ds[ds_dqf > 1] = np.nan
+    # Calculate the sum
+    sum_ds = np.nansum(np.dstack((sum_ds,ds)),2)
+    count_ds = np.nansum(np.dstack((count_ds,(ds/ds))),2)
+    #-----------------------------------------------------------------------------------------------------------
+# Calculate the sum
+ds_day = np.empty((5424,5424))
+ds_day[::] = np.nan
+ds_day[count_ds!=0] = sum_ds[count_ds!=0]/count_ds[count_ds!=0]
+#---------------------------------------------------------------------------------------------------------------------------
 # Reproject the file
 filename_ds = f'{output}/{file_name}_ret.nc'
-reproject(filename_ds, img, ds, extent, undef)
-#-----------------------------------------------------------------------------------------------------------
+reproject(filename_ds, img, ds_day, extent, undef)
+#---------------------------------------------------------------------------------------------------------------------------
 # Open the reprojected GOES-R image
 file = Dataset(filename_ds)
 
 # Get the pixel values
-data_night = file.variables['Band1'][:]
-#-----------------------------------------------------------------------------------------------------------
-# Input and output directories
-input = "Samples"; os.makedirs(input, exist_ok=True)
-output = "Output"; os.makedirs(output, exist_ok=True)
+data_geo = file.variables['Band1'][:]
 
-# Parameters to process
-product_name = 'ABI-L2-SSTF'
+# Extract date
+date = (datetime.strptime(dtime, '%Y-%m-%dT%H:%M:%S.%fZ'))
+date_formatted = date.strftime('%Y-%m-%d %H:%M')
+#---------------------------------------------------------------------------------------------------------------------------
+# Resize the array
+from skimage.transform import resize
+data_geo = resize(data_geo, (data_leo.shape[0], data_leo.shape[1]))
 
-# Desired extent
-extent = [-140.0, -60.0, -10.0, 50.0] # Min lon, Max lon, Min lat, Max lat
-
-# Date structure
-yyyymmddhhmn = '202201011500'
-
-# Download the file
-file_name = download_PROD(yyyymmddhhmn, product_name, input)
-#-----------------------------------------------------------------------------------------------------------
-# Variable
-var = 'SST'
-
-# Open the GOES-R image
-file = Dataset(f'{input}/{file_name}.nc')        
-
-# Open the file
-img = gdal.Open(f'NETCDF:{input}/{file_name}.nc:' + var)
-
-# Data Quality Flag (DQF)
-dqf = gdal.Open(f'NETCDF:{input}/{file_name}.nc:DQF')
-
-# Read the header metadata
-metadata = img.GetMetadata()
-scale = float(metadata.get(var + '#scale_factor'))
-offset = float(metadata.get(var + '#add_offset'))
-undef = float(metadata.get(var + '#_FillValue'))
-dtime = metadata.get('NC_GLOBAL#time_coverage_start')
-
-# Load the data
-ds = img.ReadAsArray(0, 0, img.RasterXSize, img.RasterYSize).astype(float)
-ds_dqf = dqf.ReadAsArray(0, 0, dqf.RasterXSize, dqf.RasterYSize).astype(float)
-
-# Apply the scale, offset and convert to celsius
-ds = (ds * scale + offset) - 273.15
-
-# Apply NaN's where the quality flag is greater than 1
-ds[ds_dqf > 1] = np.nan
-#-----------------------------------------------------------------------------------------------------------
-# Reproject the file
-filename_ds = f'{output}/{file_name}_ret.nc'
-reproject(filename_ds, img, ds, extent, undef)
-#-----------------------------------------------------------------------------------------------------------
-# Open the reprojected GOES-R image
-file = Dataset(filename_ds)
-
-# Get the pixel values
-data_day = file.variables['Band1'][:]
-#-----------------------------------------------------------------------------------------------------------
 # Calculate the difference
-data_diff = data_day - data_night
-#-----------------------------------------------------------------------------------------------------------
+data_diff = data_leo - data_geo
+#---------------------------------------------------------------------------------------------------------------------------
 # Choose the plot size (width x height, in inches)
 fig, ax = plt.subplots(figsize=(16, 8))
 
 # Plot title
-plt.title('Comparação: TSM Diurno (15:00 UTC) - Noturno (04:00 UTC)', fontsize=12, fontweight='bold')
+plt.title(f'Comparação: SST LEO e GEO - 14:00 a 20:00 UTC', fontsize=12, fontweight='bold')
 
 # X axis limits and label
-#plt.xlim(-0.1, 0.1)
+plt.xlim(-2, 2)
 plt.xlabel("Diferença (°C)",  fontsize=12, fontweight='bold')
 
 # Y axis limits and label
-plt.ylim(0, 600)
-plt.ylabel("Pixels",  fontsize=12, fontweight='bold')
+plt.ylim(0, 1000)
+plt.ylabel("N° de Pixels",  fontsize=12, fontweight='bold')
 
 # Add grids
 plt.grid(axis='x', color='0.95')
@@ -148,9 +145,3 @@ plt.grid(axis='y', color='0.95')
 
 # Show the histogram
 plt.hist(data_diff[:], facecolor='blue')
-#-----------------------------------------------------------------------------------------------------------
-# Save the image
-plt.savefig('Output/image_31.png')
-
-# Show the image
-plt.show()
---
+++ script_34.py
@@ -0,0 +1,127 @@
+#---------------------------------------------------------------------------------------------------------------------------
+# INPE / CGCT / DISSM - Training: Oceanography Products - Script 34: Significant Height and Wave Direction
+# Author: Diego Souza (INPE / CGCT / DISSM)
+#---------------------------------------------------------------------------------------------------------------------------
+# Required modules
+from netCDF4 import Dataset                # Read / Write NetCDF4 files
+import matplotlib.pyplot as plt            # Plotting library
+from datetime import datetime, timedelta   # Basic date and time types
+import cartopy, cartopy.crs as ccrs        # Plot maps
+import cartopy.feature as cfeature         # Common drawing and filtering operations
+import cartopy.io.shapereader as shpreader # Import shapefiles
+import numpy as np                         # Import the Numpy package
+import matplotlib.colors                   # Matplotlib colors  
+#---------------------------------------------------------------------------------------------------------------------------
+# File list
+files = ['WW_2021062500_hs_dironda.nc',
+         'WW_2021062600_hs_dironda.nc',
+         'WW_2021062700_hs_dironda.nc',
+         'WW_2021062800_hs_dironda.nc',
+         'WW_2021062900_hs_dironda.nc',
+         'WW_2021063000_hs_dironda.nc',
+         'WW_2021070100_hs_dironda.nc',
+         'WW_2021070200_hs_dironda.nc']
+
+for nc_file in files:
+  # Open the file using the NetCDF4 library
+  file = Dataset('Samples/' + nc_file)
+  #---------------------------------------------------------------------------------------------------------------------------
+  # Select the extent [min. lon, min. lat, max. lon, max. lat]
+  extent = [-93.0, -60.00, -25.00, 18.00] # South America
+        
+  # Reading lats and lons 
+  lats = file.variables['lat'][:]
+  lons = file.variables['lon'][:] - 360
+
+  # Latitude lower and upper index
+  latli = np.argmin( np.abs( lats - extent[1] ) )
+  latui = np.argmin( np.abs( lats - extent[3] ) )
+  
+  # Longitude lower and upper index
+  lonli = np.argmin( np.abs( lons - extent[0] ) )
+  lonui = np.argmin( np.abs( lons - extent[2] ) )
+
+  # Extract the lats and lons again
+  lats = file.variables['lat'][ latli:latui ]
+  lons = file.variables['lon'][ lonli:lonui ]
+
+  # Extract the Significant Height
+  hs = file.variables['hs'][ 0, latli:latui , lonli:lonui ]
+
+  # Extract the dir
+  dir = file.variables['dir'][ 0, latli:latui , lonli:lonui ]
+
+  # Calculate the Hx and Hy components
+  Hx = hs * np.cos(dir)
+  Hy = hs * np.sin(dir)
+
+  #---------------------------------------------------------------------------------------------------------------------------
+  # Choose the plot size (width x height, in inches)
+  plt.figure(figsize=(15,15))
+
+  # Use the Cilindrical Equidistant projection in cartopy
+  ax = plt.axes(projection=ccrs.PlateCarree())
+  ax.set_extent([extent[0], extent[2], extent[1], extent[3]], ccrs.PlateCarree())
+
+  # Define the image extent
+  img_extent = [extent[0], extent[2], extent[1], extent[3]]
+
+  # Add coastlines, borders and gridlines
+  ax.coastlines(resolution='50m', color='black', linewidth=0.8)
+  ax.add_feature(cartopy.feature.BORDERS, edgecolor='black', linewidth=0.5)
+  gl = ax.gridlines(crs=ccrs.PlateCarree(), color='white', alpha=1.0, linestyle='--', linewidth=0.25, xlocs=np.arange(-180, 180, 10), ylocs=np.arange(-90, 90, 10), draw_labels=True)
+  gl.top_labels = False
+  gl.right_labels = False
+
+  # Create a custom color scale:
+  colors = ["#0000cc", "#0166ff", "#00b6ff", "#00ffff", "#01db00", 
+            "#01ff00", "#ffff00", "#ffcc00", "#fe9900", "#ff6700", 
+            "#fe0001", "#ce1f8f", "#9c2f2b"]
+  cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors)
+  cmap.set_over('#9c2f2b')
+  cmap.set_under('#0000cc')
+  vmin = 0.0
+  vmax = 12.0
+
+  # Add a background image
+  ax.stock_img()
+
+  # Plot the image
+  img = ax.imshow(hs, vmin=vmin, vmax=vmax, origin='lower', extent=img_extent, cmap=cmap)
+
+  # Plot the quiver
+  img2 = ax.quiver(lons[::8], lats[::8], Hx[::8,::8], Hy[::8,::8], scale = 200, color='black')
+
+  # Add a shapefile
+  shapefile = list(shpreader.Reader('ne_10m_admin_1_states_provinces.shp').geometries())
+  ax.add_geometries(shapefile, ccrs.PlateCarree(), edgecolor='gray',facecolor='none', linewidth=0.3)
+
+  # Define the ticks to be shown
+  ticks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+  # Add a colorbar
+  plt.colorbar(img, label='Significant Height (m)', extend='both', orientation='vertical', pad=0.02, fraction=0.05, ticks=ticks)
+
+  year = nc_file[-24:-20]
+  month = nc_file[-20:-18]
+  day = nc_file[-18:-16]
+
+  # Getting the file time and date
+  add_hours = int(file.variables['time'][0])
+  date = datetime(int(year),int(month),int(day),0) + timedelta(hours=add_hours)
+  date_formatted = date.strftime('%Y-%m-%d %H:%M UTC')
+    
+  # Add a title
+  plt.title(f'Significant Height (m) and Wave Direction - {date_formatted}', fontweight='bold', fontsize=13, loc='left')
+  plt.title('WWATCH', fontsize=13, loc='right')
+
+  # Add a text inside the plot
+  from matplotlib.offsetbox import AnchoredText
+  text = AnchoredText("INPE / CGCT / DISSM", loc=4, prop={'size': 12}, frameon=True)
+  ax.add_artist(text)
+#--------------------------------------------------------------------------------------------------------------------------- 
+  # Save the image
+  plt.savefig('Output/' + nc_file + '.png')
+
+  # Show the image
+  plt.show()
